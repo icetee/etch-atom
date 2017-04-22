a4db0d0eac407fad0118bd1e82ec8c5485aebffb
Bat <gelezbaptiste@openmailbox.org>
2017-04-22 14:53:40 +0100

    updated package.json

---
+++ dist/etch-atom.js
@@ -0,0 +1,33 @@
+'use strict';
+'use babel';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _backgroundMessage = require('./background-message');
+
+var _editor = require('./editor');
+
+var _etchComponent = require('./etch-component');
+
+var _icon = require('./icon');
+
+var _inputView = require('./input-view');
+
+var _tabView = require('./tab-view');
+
+var _treeView = require('./tree-view');
+
+var _tree = require('./tree');
+
+exports.default = {
+  BackgroundMessage: _backgroundMessage.BackgroundMessage,
+  Editor: _editor.Editor,
+  EtchComponent: _etchComponent.EtchComponent,
+  Icon: _icon.Icon,
+  InputView: _inputView.InputView,
+  TabView: _tabView.TabView,
+  TreeView: _treeView.TreeView,
+  Tree: _tree.Tree
+};
